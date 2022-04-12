29dc30fa089f9a3e140ebed328851c8b3a0671ea
Ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-04-12 19:11:38 -0300

    requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT * FROM (SELECT title AS nome_musica,
+  CASE
+    WHEN title LIKE '%Streets%' THEN REPLACE(title, 'Streets', 'Code Review')
+    WHEN title LIKE '%Her Own%' THEN REPLACE(title, 'Her Own', 'Trybe')
+    WHEN title LIKE '%Inner Fire%' THEN REPLACE(title, 'Inner Fire', 'Project')
+    WHEN title LIKE '%Silly%' THEN REPLACE(title, 'Silly', 'Nice')
+    WHEN title LIKE '%Circus%' THEN REPLACE(title, 'Circus', 'Pull Request')
+END AS novo_nome
+FROM SpotifyClone.song) AS new_table 
+WHERE new_table.novo_nome IS NOT NULL
+ORDER BY new_table.novo_nome;
