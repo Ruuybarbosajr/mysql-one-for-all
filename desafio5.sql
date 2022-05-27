d14a505d2a1e51574f7eb8be04c3e79ed9f9df99
Ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-05-27 17:23:39 -0300

    create seeds plan users artists albums and followers

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT  s.title AS cancao, 
-        COUNT(h.user_id) AS reproducoes 
+        COUNT(h.user_id) AS reproducoes
 FROM SpotifyClone.history AS h
 JOIN SpotifyClone.song AS s
 ON s.song_id = h.song_id
-GROUP BY s.song_id 
+GROUP BY s.song_id
 ORDER BY reproducoes DESC,
 cancao LIMIT 2;
